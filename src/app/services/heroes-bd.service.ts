423b9d3b86e360bbddf0861572042fdb807ddb2f
Sarah13c <sarahcb13@hotmail.com>
2024-03-18 15:29:51 -0500

    add urlService

--- src/app/services/heroes-bd.service.ts
+++ src/app/services/heroes-bd.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,16 @@ import { Injectable } from '@angular/core';
 export class HeroesBDService {
 
   constructor(private http:HttpClient) { }
+
+  //Metodo que me trae todos los heroes
+
+
+  getHeroes(unComo:string){
+    return this.http.get(`${URL_SERVICIO_HEROES}/heroes`, {}).pipe(
+      map((res:any)=>{
+        console.log('HERORES', res);
+        return res;
+      })
+    );
+  }
 }
